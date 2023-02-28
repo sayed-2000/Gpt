11b2e9c58bb16baf3466060677d90719e4176b52
sayed aymen <117098220+sayed-2000@users.noreply.github.com>
2023-02-28 06:54:40 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import {Footer, Blog ,Possibility, Features, WhatGpt3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+        <div className="gradient__bg  ">
+          <Navbar/>
+          <Header/>
+        </div>
+        <Brand/>
+        <WhatGpt3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
